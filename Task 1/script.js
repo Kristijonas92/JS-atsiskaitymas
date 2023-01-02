826df7ca9623267c9715134317dda3ea6be06942
Kristijonas92 <kristijonas.politika@gmail.com>
2023-01-02 10:31:10 +0200

    Task 1 padarytas

--- Task 1/script.js
+++ Task 1/script.js
@@ -9,4 +9,34 @@ Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
 
+function convertWeight(kg) {
+    const pounds = kg * 2.2046;
+    const grams = kg / 0.0010000;
+    const ounces = kg * 35.274;
+    
+    return {
+      pounds: pounds,
+      grams: grams,
+      ounces: ounces
+    };
+  }
   
+  const form = document.querySelector('form');
+  
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    
+    const input = document.getElementById('search');
+    
+    const weightKg = input.value;
+    
+    const conversions = convertWeight(weightKg);
+    
+    const output = document.getElementById('output');
+    
+    output.innerHTML = `
+      <p>Weight in pounds: ${conversions.pounds}</p>
+      <p>Weight in grams: ${conversions.grams}</p>
+      <p>Weight in ounces: ${conversions.ounces}</p>
+    `;
+  });
